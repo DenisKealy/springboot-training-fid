1ec2fdef07e837c12f2c650f60ecf6a8f1628e34
Denis Kealy <deniskealyfreelance@gmail.com>
2024-08-15 14:50:53 +0000

    test fix

--- src/test/java/com/neueda/payments/service/EntityTest.java
+++ src/test/java/com/neueda/payments/service/EntityTest.java
@@ -4,7 +4,16 @@ import com.neueda.payments.model.Payment;
 import com.neueda.payments.repositories.PaymentsRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class EntityTest {
 
     private PaymentsRepository paymentsRepository;
